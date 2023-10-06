45eb042aab01188aa728aa2876dcea1f68e138ee
Nicorabi <121673697+Nicorabi@users.noreply.github.com>
2023-10-06 14:26:29 +1100

    Edit js

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ let containerParagraphOne = document.getElementById('container-paragraph-one');
 let containerParagraphTwo = document.getElementById('container-paragraph-two');
 let buttonOne = document.getElementById('button-one');
 
-function randomPasswordOne() {
-    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
-    let password = '';
-    for (let i = 0; i < 8; i++) {
-        let randomIndex = Math.floor(Math.random() * str.length);
-        password += str[randomIndex];
-    }
-    return password;
-}
-
-function randomPasswordTwo() {
+function randomPassword() {
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
     let password = '';
     for (let i = 0; i < 8; i++) {
@@ -23,6 +13,6 @@ function randomPasswordTwo() {
 }
 
 function render() {
-    containerParagraphOne.textContent = randomPasswordOne();;
-    containerParagraphTwo.textContent = randomPasswordTwo();
+    containerParagraphOne.textContent = randomPassword();;
+    containerParagraphTwo.textContent = randomPassword();
 }
